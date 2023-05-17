b8dda4c5e7211b9a6d1feec50f4106977afeb84c
Ezell <lynellf@gmail.com>
2023-05-17 09:03:22 -0500

    fix uuid

--- utils/uniqueID.ts
+++ utils/uniqueID.ts
@@ -1,5 +1,10 @@
 import uniqueid from "uniqueid";
 
 export default function uniqueID(size = 21) {
-  return uniqueid()();
+  // GPT-4 generated
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0,
+      v = c == "x" ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
